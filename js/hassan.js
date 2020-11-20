60e7a4e2afd2410af5da09e45ba4948585dda007
hassanblog <1069171387@qq.com>
2020-11-21 00:50:46 +0800

    Site updated: 2020-11-21 00:50:44

--- js/hassan.js
+++ js/hassan.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>// 设置页脚博主会动的心
+// 设置页脚博主会动的心
 // $(document).ready(function (e) {
 //     $('.copyright').html('©2019 - 2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Hassan Wong');
 // })
