6cb5b9c5aa2cabc52c21702d97ffb5f612aaab98
hassanblog <1069171387@qq.com>
2020-12-03 23:58:50 +0800

    Site updated: 2020-12-03 23:58:42

--- hassan/hassan.js
+++ hassan/hassan.js
@@ -1,22 +1,22 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>// 设置页脚博主会动的心
-// $(document).ready(function (e) {
-//     $('.copyright').html('©2019 - 2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Hassan Wong');
-// })
+设置页脚博主会动的心
+$(document).ready(function (e) {
+    $('.copyright').html('©2019 - 2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Hassan Wong');
+})
 
 
-// // 卖萌标题
-// var OriginTitle = document.title;
-// var titleTime;
-// document.addEventListener('visibilitychange', function() {
-//     if (document.hidden) {
-//         $('[rel="icon"]').attr('href', "/img/favicon.ico");
-//         document.title = '(つェ⊂) 感觉你怪怪的~~';
-//         clearTimeout(titleTime);
-//     } else {
-//         $('[rel="icon"]').attr('href', "/img/favicon.ico");
-//         document.title = '(*´∇｀*) 怪可爱的~~' + OriginTitle;
-//         titleTime = setTimeout(function() {
-//             document.title = OriginTitle;
-//         }, 2000);
-//     }
-// });
+// 卖萌标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(つェ⊂) 感觉你怪怪的~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(*´∇｀*) 怪可爱的~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
